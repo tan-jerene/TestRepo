ef44ad1bdd6c3faabfe8105edad25f1f5ac0aa97
Jerene Ricketts <jerene.ricketts@gmail.com>
2017-08-09 14:12:35 -0500

    New Dummy files and directories

---
+++ TestDir/DummyClass.java
@@ -0,0 +1,5 @@
+public class DummyClass{
+	public static void main(String[] args){
+		System.out.println("Random Dummy Class");
+	}
+}
---
+++ TestDir/TestDir2/DummyClass2.java
@@ -0,0 +1,5 @@
+public class DummyClass2{
+	public static void main(String[] args){
+		System.out.println("Second Random Dummy Class");
+	}
+}
